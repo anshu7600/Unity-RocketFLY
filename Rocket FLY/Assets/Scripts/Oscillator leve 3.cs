dfaf115fa3eaf3b617f95eaa3bce9048d2093f58
ShreyanshVennamaneni <144498974+anshu7600@users.noreply.github.com>
2024-04-12 17:45:41 -0500

    Started on the cinemachine camera level 4

--- Rocket FLY/Assets/Scripts/Oscillator leve 3.cs
+++ Rocket FLY/Assets/Scripts/Oscillator leve 3.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Oscillator : MonoBehaviour
+public class Oscillator1 : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
